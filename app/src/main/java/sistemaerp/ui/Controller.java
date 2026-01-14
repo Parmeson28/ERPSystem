01271dec5c929edd6defc99c703cb00dbb9638c5
Tiago Eden <tiagopassigern2802@gmail.com>
2026-01-14 08:18:38 -0300

    Now you can switch scenes to register new productos or search for the ones registered

--- app/src/main/java/sistemaerp/ui/Controller.java
+++ app/src/main/java/sistemaerp/ui/Controller.java
@@ -1,9 +1,16 @@
 package sistemaerp.ui;
 
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller {
 
@@ -12,6 +19,10 @@ public class Controller {
 
    TextField descricao, codigo;
 
+   private Stage stage;
+   private Scene scene;
+   private Parent root;
+
    public void takeText(ActionEvent e){
         
         System.out.println(descricao.getText());
@@ -19,6 +30,22 @@ public class Controller {
 
         descricao.setText("");
         codigo.setText("");
+   }
+
+   public void switchMain(ActionEvent event) throws IOException{
+      root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+      stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+      scene = new Scene(root);
+      stage.setScene(scene);
+      stage.show();
+   }
+
+   public void switchCadastro(ActionEvent event) throws IOException{
+      root = FXMLLoader.load(getClass().getResource("/cadastrarProdutos.fxml"));
+      stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+      scene = new Scene(root);
+      stage.setScene(scene); 
+      stage.show();
    }
 
 }
