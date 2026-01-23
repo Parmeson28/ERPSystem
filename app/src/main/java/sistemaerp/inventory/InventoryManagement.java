1fb3ef642abd64c1d391e194d2ee4aa5550d1551
Parmeson28 <tiagopassigern2802@gmail.com>
2026-01-22 22:40:20 -0300

    Connecting to database. Fixed connection with txt file

--- app/src/main/java/sistemaerp/inventory/InventoryManagement.java
+++ app/src/main/java/sistemaerp/inventory/InventoryManagement.java
@@ -15,8 +15,6 @@ public class InventoryManagement {
     
     private ArrayList<String> products = new ArrayList<>();
 
-    private String filePath = "app\\src\\main\\resources\\inventory.txt";
-
     BufferedReader reader;
     BufferedWriter writer;
     String line;
@@ -33,81 +31,18 @@ public class InventoryManagement {
     }
 
    public ArrayList<String> showItems(){
-    
-        try {
-
-            while((line = reader.readLine()) != null){
-
-                line = line.replace(";", " ");
-                
-                products.add(line);
-
-            }  
-
-            return products;
-
-        }catch(FileNotFoundException e) {
-            System.out.println("ERRO");
-            return new ArrayList<>();
-            
-        }catch(IOException a){
-            System.out.println("ERROR");
-            return new ArrayList<>();
-        }
-
+        return new ArrayList<String>();
    }
 
 
    public void registerItems(String description, String id, int quantity){
-        try {
-            writer = new BufferedWriter(new FileWriter(filePath, true));
-            
-
-            if(!description.strip().equals("") && !id.strip().equals("")){
-
-                line = id+";"+description+";"+quantity;
-
-                writer.write(line);
-                writer.newLine();
-            }
-           
-
-            writer.flush();
-            writer.close();
-
-        }catch(FileNotFoundException e) {
-            System.out.println("ERRO");
-            
-        }catch(IOException a){
-            System.out.println("ERROR");
-        }
+        
    }
 
 
    public void searchForItem(String description, String id){
 
-        try {
-
-            while((line = reader.readLine()) != null){
-
-                line = line.replace(";", " ");
-                String[] lineArray = line.split(" ");
-
-                if(lineArray[0].equals(id) || lineArray[1].equals(description)){
-                    System.out.println("DEu boa");
-                }else{
-                    System.out.println("Nem pa");
-                }
-                
-            }  
-
-        }catch(FileNotFoundException e) {
-            System.out.println("Games");
-            
-        }catch(IOException a){
-            System.out.println("Nao games");
-        }
-
+        
    }
 
 }
